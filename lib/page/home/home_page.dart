0fc6922b1581541c38459cfae52fc83429224d9b
droidYu <droidYu@foxmail.com>
2023-03-29 13:09:23 +0800

    SliverList

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -11,36 +11,40 @@ class HomePage extends GetView<HomeController> {
   Widget build(BuildContext context) {
     Get.put(HomeController());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Demo'),
-        actions: [
-          IconButton(
-              tooltip: 'setting'.tr,
-              onPressed: () {
-                Get.to(() => const SettingPage());
-              },
-              icon: const Icon(Icons.settings))
-        ],
-      ),
-      body: Obx(
-        ()=> SmartRefresher(
-          header: const WaterDropHeader(),
-          controller: controller.refreshController,
-          enablePullUp: true,
-          onRefresh: (){
-            controller.refreshData();
-          },
-          onLoading: (){
-            controller.loadMoreData();
-          },
-          child: ListView.builder(
-            itemCount: controller.list.length,
-            itemBuilder: (c, i) => ListTile(
-              title: Text(controller.list[i].title ?? ''),
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text('Demo'),
+          actions: [
+            IconButton(
+                tooltip: 'setting'.tr,
+                onPressed: () {
+                  Get.to(() => const SettingPage());
+                },
+                icon: const Icon(Icons.settings))
+          ],
         ),
-      )
-    );
+        body: Obx(
+          () => SmartRefresher(
+              header: const WaterDropHeader(),
+              controller: controller.refreshController,
+              enablePullUp: true,
+              onRefresh: () {
+                controller.refreshData();
+              },
+              onLoading: () {
+                controller.loadMoreData();
+              },
+              child: CustomScrollView(
+                slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      childCount: controller.list.length,
+                      (context, index) => ListTile(
+                        title: Text(controller.list[index].title ?? ''),
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ));
   }
 }
